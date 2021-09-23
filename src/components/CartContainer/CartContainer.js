408de80115873126118ebe08c5dd63cafe56655e
Alexander <AlexanderRy@protonmail.com>
2021-09-23 18:14:56 +0300

    added message to display if the cart if the cart is empty

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -6,69 +6,84 @@ import { increment } from "../../my-redux/actions";
 import { decrement } from "../../my-redux/actions";
 import { submit } from "../../my-redux/actions";
 
-export class CartContainer extends Component {
-  render() {
-    let cartItem = "";
-    if (this.props.cart) {
-      cartItem = this.props.cart.map((item) => (
-        <div className="cart-item-row" key={item.id}>
-          <div className="image-block">
-            <img className="mini-image" src={item.image} alt=""></img>
+
+
+
+export  function CartContainer(props) {
+
+  let cartItem = "";
+  if (props.cart) {
+    cartItem = props.cart.map((item) => (
+      <div className="cart-item-row" key={item.id}>
+        <div className="image-block">
+          <img className="mini-image" src={item.image} alt=""></img>
+        </div>
+
+        <div className="information-block">
+          <div className="description-block">
+            <p className="cart-description">{item.description}</p>
+
+            <button
+              className="remove-item"
+              onClick={() => props.removeFromCart(item)}
+            ></button>
           </div>
 
-          <div className="information-block">
-            <div className="description-block">
-              <p className="cart-description">{item.description}</p>
-
+          <div className="price-block">
+            <div className="item-price">{item.price + "₴"}</div>
+            <div className="quantity-block">
               <button
-                className="remove-item"
-                onClick={() => this.props.removeFromCart(item)}
-              ></button>
+                className="plus-button"
+                onClick={() => props.increment(item)}
+              >
+                +
+              </button>
+              <div className="item-quantity">{item.quantity}</div>
+              <button
+                className="minus-button"
+                onClick={() => props.decrement(item)}
+              >
+                -
+              </button>
             </div>
-
-            <div className="price-block">
-              <div className="item-price">{item.price + "₴"}</div>
-              <div className="quantity-block">
-                <button
-                  className="plus-button"
-                  onClick={() => this.props.increment(item)}
-                >
-                  +
-                </button>
-                <div className="item-quantity">{item.quantity}</div>
-                <button
-                  className="minus-button"
-                  onClick={() => this.props.decrement(item)}
-                >
-                  -
-                </button>
-              </div>
-              <div className="item-total-price">
-                {item.price * item.quantity + "₴"}
-              </div>
+            <div className="item-total-price">
+              {item.price * item.quantity + "₴"}
             </div>
           </div>
         </div>
-      ));
-    }
-    let cartSum = this.props.cart.reduce(function (total, item) {
-      return total + item.price * item.quantity;
-    }, 0);
-    return (
-      <main>
-        {cartItem}
-        <div className="total-sum-block">Total sum -{cartSum} </div>
-        <button
-          type="submit"
-          className="submit-form-button"
-          onClick={() => this.props.submit()}
-        >
-          Submit
-        </button>
-      </main>
-    );
+      </div>
+    ));
   }
+  let cartSum = props.cart.reduce(function (total, item) {
+    return total + item.price * item.quantity;
+    
+  }, 0);
+  if (cartSum<1){
+    cartSum="The cart is empty"
+  }
+  else{
+    cartSum="total sum is "+cartSum
+  }
+
+
+  return (
+   
+    <main>
+    {cartItem}
+    <div className="total-sum-block">{cartSum} </div>
+    <button
+      type="submit"
+      className="submit-form-button"
+      onClick={() => props.submit()}
+    >
+      Submit
+    </button>
+  </main>
+
+  )
 }
+
+
 const mapStateToProps = (state) => {
   return {
     cart: state.cart,
