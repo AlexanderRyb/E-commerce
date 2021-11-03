999b252cda6d3f43cbb3b86aec8df10b6158d5f2
Alexander <AlexanderRy@protonmail.com>
2021-11-03 11:41:30 +0200

    basic form

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -62,6 +62,21 @@ export function CartContainer(props) {
   return (
     <main>
       <form id="cart-form">
+<label htmlFor="customer-name">Name</label>
+<input id="customer-name" className="form-input"></input>
+<br/>
+<label htmlFor="customer-surname">second name</label>
+<input htmlFor="customer-surname" className="form-input"></input>
+<br/>
+
+
+<label htmlFor="customer-phone">phone</label>
+<input id="customer-phone" className="form-input"></input>
+<br/>
+
+
+<label htmlFor="customer-address">address</label>
+<input id="customer-address" className="form-input"></input>
 
 
       </form>
